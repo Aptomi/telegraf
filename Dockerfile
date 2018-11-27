ed5c726501536f7f040ae9e0bc1c447962488faa
meta coma <m3tac0ma@gmail.com>
2018-11-27 19:43:51 +0400

    fix influx line output

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,4 @@ FROM telegraf:1.8
 COPY --from=consul /bin/consul /usr/local/bin/consul
 COPY --from=concourse /opt/resource/fly /usr/local/bin/fly
 ADD bin/concourse_workers.sh /usr/local/bin/
+RUN apt update && apt install -y gawk
--- bin/concourse_workers.sh
+++ bin/concourse_workers.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+set -e
+(fly -t main status || fly -t main login -c ${CONCOURSE_URL} -u ${CONCOURSE_USERNAME} -p ${CONCOURSE_PASSWORD}) >/dev/null 2>&1
+fly -t main workers |  awk -F" " '{
+	printf("concourse,worker=%s state=%s,containers=%s %d000000000\n", $1, ($6 == "2running") ? 1 : 0, $2, systime())
+}'
 
-fly -t main status || fly -t main login -c ${CONCOURSE_URL} -u ${CONCOURSE_USERNAME} -p ${CONCOURSE_PASSWORD}
-fly -t main workers | awk -F" " '{printf("concourse,worker=%s containers=%d state=%s\n", $1, $2, $6)}'
