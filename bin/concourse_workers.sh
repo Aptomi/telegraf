acb47ae9c969e03f0c12eb00e075620c68168914
meta coma <m3tac0ma@gmail.com>
2018-11-28 18:09:21 +0400

    fix typo in container state

--- bin/concourse_workers.sh
+++ bin/concourse_workers.sh
@@ -2,6 +2,6 @@
 set -e
 (fly -t main status || fly -t main login -c ${CONCOURSE_URL} -u ${CONCOURSE_USERNAME} -p ${CONCOURSE_PASSWORD}) >/dev/null 2>&1
 fly -t main workers |  awk -F" " '{
-	printf("concourse,worker=%s state=%s,containers=%s %d000000000\n", $1, ($6 == "2running") ? 1 : 0, $2, systime())
+	printf("concourse,worker=%s state=%s,containers=%s %d000000000\n", $1, ($6 == "running") ? 1 : 0, $2, systime())
 }'
 
