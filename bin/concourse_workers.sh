7eb0478a2db3ce881757aaa3e7e27bd8b630683e
meta coma <m3tac0ma@gmail.com>
2018-11-30 19:42:11 +0400

    added test

--- bin/concourse_workers.sh
+++ bin/concourse_workers.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 set -e
-(fly -t main status || fly -t main login -c ${CONCOURSE_URL} -u ${CONCOURSE_USERNAME} -p ${CONCOURSE_PASSWORD}) >/dev/null 2>&1
-fly -t main workers |  awk -F" " '{
-	printf("concourse,worker=%s state=%s,containers=%s %d000000000\n", $1, ($6 == "running") ? 1 : 0, $2, systime())
-}'
+
+source()  {
+        (fly -t main status || fly -t main login -c ${CONCOURSE_URL} -u ${CONCOURSE_USERNAME} -p ${CONCOURSE_PASSWORD}) >/dev/null 2>&1
+        fly -t main workers
+}
+
+fake_source() {
+        cat<<EOF
+concourse-worker-0  0  linux  none  none  running  2.1
+concourse-worker-1  1  linux  none  none  runni2ng  2.1
+concourse-worker-2  1  linux  none  none  running  2.1
+EOF
+}
+
+parse() {
+        awk -F" " '{
+                printf("concourse,worker=%s state=%s,containers=%s %d000000000\n", $1, ($6 == "running") ? 1 : 0, $2, systime())
+        }'
+}
+
+
+source | parse
 
