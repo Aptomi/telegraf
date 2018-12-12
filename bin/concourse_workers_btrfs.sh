2c577b99a6da14ad29dadbafda1afca2d9f3e946
meta coma <m3tac0ma@gmail.com>
2018-12-12 21:36:35 +0400

    fix btrs naming

--- bin/concourse_workers_btrfs.sh
+++ bin/concourse_workers_btrfs.sh
@@ -3,14 +3,14 @@ set -e
 
 
 concourse_worker_list() {
-	kubectl -n ${CONCOURSE_NS} get pods -l release=concourse,app=concourse-worker -o jsonpath='{.items[*].metadata.name}' 
-} 
+	kubectl -n ${CONCOURSE_NS} get pods -l release=concourse,app=concourse-worker -o jsonpath='{.items[*].metadata.name}'
+}
 
-concourse_worker_brtfs_du() {
+concourse_worker_btrfs_du() {
 	local pod_name=$1
 	local cmd="/concourse-work-dir/${CONCOURSE_VERSION}/assets/btrfs/btrfs filesystem usage --raw /concourse-work-dir/volumes"
 	kubectl -n ${CONCOURSE_NS} exec -ti ${pod_name} -- ${cmd}
-} 
+}
 
 
 btrfs_parse() {
@@ -23,12 +23,12 @@ btrfs_parse() {
 		free=$3
 	}
 	END {
-		printf("concourse,worker=%s size=%d,free=%d,usage_percent=%d\n", pod_name, size, free, (size-free)*100/size )	
+		printf("concourse_btrfs,worker=%s size=%d,free=%d,usage_percent=%d\n", pod_name, size, free, (size-free)*100/size )
 	}
 '
-} 
+}
 
-KUBECONFIG=/tmp/kubeconfig
+export KUBECONFIG=/tmp/kubeconfig
 CONCOURSE_NS=concourse
 CONCOURSE_VERSION="4.2.1"
 
@@ -37,7 +37,7 @@ test -f ${KUBECONFIG} || echo "${KUBECONFIG_DATA}" | base64 -d > ${KUBECONFIG}
 
 kubectl version > /dev/null 2>&1
 
-for pod in $(concourse_worker_list); do 	
-	concourse_worker_brtfs_du ${pod} | btrfs_parse ${pod}
-		
+for pod in $(concourse_worker_list); do
+	concourse_worker_btrfs_du ${pod} | btrfs_parse ${pod}
+
 done
